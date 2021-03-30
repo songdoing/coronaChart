0353728d17a1fe8e06ea4775ef6d973bff2aa0df
wonjinsong <msmk22239@gmail.com>
2021-03-29 22:57:23 -0400

    cumulative cases fixed 1

--- src/components/contents.js
+++ src/components/contents.js
@@ -3,17 +3,7 @@ import { Bar, Doughnut, Line } from "react-chartjs-2";
 import axios from "axios";
 
 const Contents = () => {
-  const [confirmedData, setConfirmedData] = useState({
-    labels: [],
-    datasets: [
-      {
-        label: "",
-        backgroundColor: "",
-        fill: true,
-        data: [],
-      },
-    ],
-  });
+  const [confirmedData, setConfirmedData] = useState({});
   useEffect(() => {
     //데이터를 다 받은 다음에 순차적으로 실행하도록 async와 await
     const fetchEvents = async () => {
@@ -29,18 +19,17 @@ const Contents = () => {
         const year = currentDate.getFullYear();
         const month = currentDate.getMonth();
         const date = currentDate.getDate();
-        console.log(year, month, date);
+        //console.log(year, month, date);
+        //console.log(cur);
         const confirmed = cur.Confirmed;
         const active = cur.Active;
         const death = cur.Deaths;
         const recovered = cur.Recovered;
 
-        const findItem = acc.find(
-          (a) => a.year === year && a.month === month && a.date === date
-        );
+        const findItem = acc.find((a) => a.year === year && a.month === month);
 
         if (!findItem) {
-          //key value가 같으면 생략 가능
+          //key:value가 같으면 생략 가능 {year:year, month:month...}
           acc.push({ year, month, date, confirmed, active, death, recovered });
         }
         if (findItem && findItem.date < date) {
@@ -58,7 +47,7 @@ const Contents = () => {
       }, []);
       console.log(arr);
 
-      const labels = arr.map((a) => `${a.month + 1}`);
+      const labels = arr.map((a) => `${a.year} - ${a.month + 1}`);
       setConfirmedData({
         labels,
         dataset: [
