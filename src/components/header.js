638e344e5a7bbc87798c164ddec29c31cd152c93
wonjinsong <msmk22239@gmail.com>
2021-04-08 23:51:15 -0400

    gh-pages

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <header className="header">
-      <h1>COVID-19</h1>
+      <h1>COVID-19 : CANADA</h1>
       {/* <select>
         <option>CANADA</option>
         <option>WORLD</option>
